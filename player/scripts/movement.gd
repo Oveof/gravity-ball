b7bcb1fb78776213b62d5424886d3c0ae8a4b54a
Ove Oftedal <ovedy@oftedal.com>
2026-03-30 14:26:23 +0200

    feat: moved player to own domain, and added gravity based movement

---
+++ player/scripts/movement.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+var speed = 2;
+var gravity_direction = 1;
+var gravity_speed = Vector2.DOWN * 1;
+const GRAVITY_MULTIPLIER = 0.02
+
+func get_input():
+	if Input.is_action_just_pressed("activate_gravity"):
+		gravity_direction *= -1;
+		gravity_speed = Vector2.DOWN * 1
+
+	if Input.is_action_pressed("activate_gravity"):
+		gravity_speed += Vector2.DOWN * GRAVITY_MULTIPLIER;
+
+
+
+func _physics_process(delta):
+	get_input()
+	var gravity_vec = gravity_speed * gravity_direction;
+	velocity = velocity.move_toward(Vector2.RIGHT, speed) + gravity_vec;
+	move_and_collide(velocity)
